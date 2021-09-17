bb8b01fdd53a1a8653a13403b2d8e38ba80b5901
jair429 <jterrones@programate.pe>
2021-09-17 16:35:17 -0500

    Agregando controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,28 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Bienvenido a la página principal";
-});
 
-Route::get('cursos',function(){
-    return "Bienvenido a cursos";
-});
+Route::get('/',HomeController::class);
 
-Route::get('cursos/create', function () {
-    return "En esta página podras crear un curso";
-});
+Route::get('cursos',[CursoController::class, 'index']);
 
-// Route::get('cursos/{curso}', function ($curso) {
-//     return "Bienvenido al curso: $curso";
+Route::get('cursos/create', [CursoController::class,'create']);
+
+Route::get('cursos/{curso}',[CursoController::class,'show']);
+
+// Route::get('cursos/{cursos}/{categoria?}', function ($curso,$categoria = null) {
+
+//     if($categoria){
+//         return "Bienvenido al curso $curso, de la categoria $categoria";
+//     }else{
+//         return "Bienvenido al curso: $curso";
+//     }
 // });
-
-Route::get('cursos/{cursos}/{categoria?}', function ($curso,$categoria = null) {
-
-    if($categoria){
-        return "Bienvenido al curso $curso, de la categoria $categoria";
-    }else{
-        return "Bienvenido al curso: $curso";
-    }
-});
