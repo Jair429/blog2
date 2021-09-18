6d8bb18a880a2209e9fe9c7b859f07b5100b1699
jair429 <jterrones@programate.pe>
2021-09-17 19:19:31 -0500

    Agregando vista a un controllador

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index(){
-        return "Bienvenido a la página principal";
+        return view('cursos.index');
     }
 
     public function create(){
-        return "En esta página podras crear un curso";
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return "Bienvenido al curso: $curso";
+        // return view('cursos.show',['curso' => $curso]);
+        return view('cursos.show', compact('curso'));
     }
 }
