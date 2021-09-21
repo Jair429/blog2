0e3ade42afccf44b2cd47d77f9efe263d37230bf
jair429 <jterrones@programate.pe>
2021-09-21 11:07:50 -0500

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Curso;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        $this->call(CursoSeeder::class);
+        Curso::factory(50)->create();
     }
 }
